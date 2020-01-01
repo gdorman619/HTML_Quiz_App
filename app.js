be7e14b6ca859dc012f36a013af775a5807bf4f2
Gary Dorman <garydorman@Garys-MacBook-Pro.local>
2020-01-01 17:12:57 -0500

    added some more css

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
+document
+  .querySelector("#choice_1")
+  .addEventListener("click", function() {
+    document.querySelector("#choice_1").style.background = "#ff6666";
+  });
 
+document
+  .querySelector("#choice_2")
+  .addEventListener("click", function() {
+    document.querySelector("#choice_2").style.background = "#ff6666";
+  });
+
+document
+  .querySelector("#choice_3")
+  .addEventListener("click", function() {
+    document.querySelector("#choice_3").style.background = "#ff6666";
+  });
+
+document
+  .querySelector("#choice_4")
+  .addEventListener("click", function() {
+    document.querySelector("#choice_4").style.background = "#ff6666";
+  });
