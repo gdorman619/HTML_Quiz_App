7c140b6cc318d6d7346c34f0edd4579b0acc49b3
Gary Dorman <garydorman@Garys-MacBook-Pro.local>
2020-01-01 23:44:29 -0500

    updated some javascript

--- app.js
+++ app.js
@@ -1,23 +1,30 @@
 document
   .querySelector("#choice_1")
   .addEventListener("click", function() {
-    document.querySelector("#choice_1").style.background = "#ff6666";
+    document.getElementById("choice_1").style.color = "purple";
+    document.getElementById("choice_1").style.background = "magenta";
   });
 
 document
   .querySelector("#choice_2")
   .addEventListener("click", function() {
-    document.querySelector("#choice_2").style.background = "#ff6666";
+    document.getElementById("choice_2").style.color = "purple";
   });
 
 document
   .querySelector("#choice_3")
   .addEventListener("click", function() {
+    document.getElementById("choice_3").style.color = "purple";
     document.querySelector("#choice_3").style.background = "#ff6666";
   });
 
 document
   .querySelector("#choice_4")
   .addEventListener("click", function() {
+    document.getElementById("choice_4").style.color = "purple";
     document.querySelector("#choice_4").style.background = "#ff6666";
   });
+
+let choices = document.getElementsByClassName("choice_text");
+
+console.log(choices[0].textContent);
